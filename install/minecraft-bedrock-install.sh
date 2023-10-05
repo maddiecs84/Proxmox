d4ffc92a66203179fecd4b98ad3ba3bf63970e26
Maddie Carter-Smith <maddie@carter-smith.uk>
2023-10-05 19:02:24 +0100

    use std

--- install/minecraft-bedrock-install.sh
+++ install/minecraft-bedrock-install.sh
@@ -28,7 +28,7 @@ $STD rc-update add docker default
 msg_ok "Installed Docker"
 
 msg_info "Starting Minecraft Bedrock"
-docker run -d -it -e EULA=TRUE -p 19132:19132/udp -v mc-bedrock-data:/data itzg/minecraft-bedrock-server
+$STD docker run -d -it -e EULA=TRUE -p 19132:19132/udp -v mc-bedrock-data:/data itzg/minecraft-bedrock-server
 msg_ok "Started Minecraft Bedrock"
 
 motd_ssh
