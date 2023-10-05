b0bcf751c9543a011f83616cb984db4714970a00
Maddie Carter-Smith <maddie@carter-smith.uk>
2023-10-05 19:03:29 +0100

    wait for docker to start

--- install/minecraft-bedrock-install.sh
+++ install/minecraft-bedrock-install.sh
@@ -28,6 +28,11 @@ $STD rc-update add docker default
 msg_ok "Installed Docker"
 
 msg_info "Starting Minecraft Bedrock"
+while (! docker stats --no-stream ); do
+  # Docker takes a few seconds to initialize
+  echo "Waiting for Docker to launch..."
+  sleep 1
+done
 $STD docker run -d -it -e EULA=TRUE -p 19132:19132/udp -v mc-bedrock-data:/data itzg/minecraft-bedrock-server
 msg_ok "Started Minecraft Bedrock"
 
