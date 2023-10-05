524686cc7af329c100f2f070280659a01a77670e
Maddie Carter-Smith <maddie@carter-smith.uk>
2023-10-05 18:58:48 +0100

    Confirming didnt cause exit code 125

--- install/minecraft-bedrock-install.sh
+++ install/minecraft-bedrock-install.sh
@@ -28,8 +28,8 @@ $STD rc-update add docker default
 msg_ok "Installed Docker"
 
 msg_info "Starting Minecraft Bedrock"
-docker run -d -it --restart unless-stopped -e EULA=TRUE -p 19132:19132/udp -v mc-bedrock-data:/data itzg/minecraft-bedrock-server
-msg_info "Started Minecraft Bedrock"
+docker run -d -it -e EULA=TRUE -p 19132:19132/udp -v mc-bedrock-data:/data itzg/minecraft-bedrock-server
+msg_ok "Started Minecraft Bedrock"
 
 motd_ssh
 customize
