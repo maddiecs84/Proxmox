f0af25ac1940101c17f41fe095a9ed47c2878b87
Maddie Carter-Smith <maddie@carter-smith.uk>
2023-10-05 18:50:34 +0100

    install the bedrock server

--- install/minecraft-bedrock-install.sh
+++ install/minecraft-bedrock-install.sh
@@ -27,23 +27,9 @@ $STD rc-service docker start
 $STD rc-update add docker default
 msg_ok "Installed Docker"
 
-get_latest_release() {
-  curl -sL https://api.github.com/repos/$1/releases/latest | grep '"tag_name":' | cut -d'"' -f4
-}
-DOCKER_COMPOSE_LATEST_VERSION=$(get_latest_release "docker/compose")
-
-read -r -p "Would you like to add Docker Compose? <y/N> " prompt
-if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
-  msg_info "Installing Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
-  DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-  mkdir -p $DOCKER_CONFIG/cli-plugins
-  curl -sSL https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_LATEST_VERSION/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-  chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-  msg_ok "Installed Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
-
-
-  # $STD docker run -d -it -e EULA=TRUE -p 19132:19132/udp -v mc-bedrock-data:/data itzg/minecraft-bedrock-server
-fi
+msg_info "Starting Minecraft Bedrock"
+$STD docker run -d -it --restart unless-stopped -e EULA=TRUE -p 19132:19132/udp -v mc-bedrock-data:/data itzg/minecraft-bedrock-server
+msg_info "Started Minecraft Bedrock"
 
 motd_ssh
 customize
